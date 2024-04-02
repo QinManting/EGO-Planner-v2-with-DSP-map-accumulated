412b2b3f083c8a9f5bd8d7b0f2c0d0b65974264b
覃漫婷20B <2295547681@qq.com>
2024-04-02 22:21:28 +0800

    更新代码

--- swarm-playground/main_ws/build/Utils/pose_utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qinmanting/Arial_learning/ego-v2+map_generator/src/EGO-Planner-v2/swarm-playground/main_ws/src/Utils/pose_utils
-# Build directory: /home/qinmanting/Arial_learning/ego-v2+map_generator/src/EGO-Planner-v2/swarm-playground/main_ws/build/Utils/pose_utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- swarm-playground/main_ws/build/planner/path_searching/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qinmanting/Arial_learning/ego-v2+map_generator/src/EGO-Planner-v2/swarm-playground/main_ws/src/planner/path_searching
-# Build directory: /home/qinmanting/Arial_learning/ego-v2+map_generator/src/EGO-Planner-v2/swarm-playground/main_ws/build/planner/path_searching
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
